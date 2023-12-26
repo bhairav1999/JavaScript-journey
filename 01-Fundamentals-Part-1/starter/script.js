0787c0fb616883605d640ae05013bfdd20640fdd
Bhairav Karande <123438414+bhairav1999@users.noreply.github.com>
2023-12-26 12:23:08 +0530

    guess my number

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+
+
+let js = "variable"; // use camelCase 
+
+console.log(20+56-59*8);
+
+// const 3yeesr = "i am join"  // error  variable cannot start number 
+
+// variable start only letters, underscores, or the dollar sign.
+
+const _hello ="hello"  // this is correct
+
+const $dooler = "dooler" // this is correct
+
+const letter = "hello" // this is correct
+
+const data_underscore = "abcdefghijklmnopqrstuvwxyz" // this is correct
+
+
+//<----------------------------------------------------------------------------------------------->//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+ document.querySelector(".message")
+ console.log(`Bhairav`, document.querySelector(".message").textContent)
+ console.log(`Bhairav`, document.querySelector(".message").innerHTML)
+ document.querySelector(".message").textContent ="✨ Correct Number"
+
+
+ document.querySelector(".number").textContent=19;
+ document.querySelector(".score").textContent =25;
+
+ document.querySelector(".guess").value = 475;
+
+
+ console.log(document.querySelector(".guess").value =474)
+
